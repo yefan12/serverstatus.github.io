fbbe4d4087bbbc729515bc167aadced9f8abab6f
Nuub1es <98951177+Nuub1es@users.noreply.github.com>
2022-03-24 18:08:33 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Evolution Server Monitor</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+    <meta name="viewport" content="with=device-width, user-scalable=yes, initial-scale=1.0"/>
+
+    <meta name="description" content="Evolution Roleplay adalah Server SA:MP Mode Roleplay" />
+</head>
+<body>
+     <form action="index.php" method="post">
+</body>
+</html>
+<strong>
+	<p>Evolution Roleplay</strong>
+<form/>
+    <br>
+    <br>
+	HostName :  Evolution Roleplay #Modernization
+    <br>
+     Address :  evolution-rp.duckdns.org:9200
+     <br>
+     Players :  XX / XX
+     <br>
+     Ping    :  XX
+     <br>
+     Mode    :  EVO:RP v2.12.3r3
+     <br>
+     Language:  Bahasa Indonesia
+    <br>
+    <br>
+    <strong>Status Server :  ONLINE !</strong>
+<br>
+<br>
+	<strong>Join Discord Evolution</strong>
+</p>
+<p class="itemtext">
+  <a href="https://dsc.gg/gtaevolution">Discord</a>
+</p>
+  <strong>Evolution SA:MP</strong>
+  <br>
+  <br>
+  <a href="samp://evolution-rp.duckdns.org:9002">Click To play</a>
+  <br>
+  <br>
+        <br>
+<div class="footer-dark">
+        <footer>
+            <div class="container">
+                <p class="powered"></p>
+                <p class="copyright">© Team Evolution 2022 </p>
+        </footer>
+    </div>
+</p>
+</body>
+</html>
+</body>
+</html>
